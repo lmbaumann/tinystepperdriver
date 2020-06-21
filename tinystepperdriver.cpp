4023e10004c3a40b2e31f2bc21981f0a8866821a
Luca Baumann <luccaa7@web.de>
2020-06-21 02:29:53 +0200

    Generally sort-of works added lots of comments added test notebook for python bindings

--- tinystepperdriver.cpp
+++ tinystepperdriver.cpp
@@ -1,19 +1,25 @@
+/*
+* tinystepperdriver.cpp - Lightweight library for stepper motor controll on arduino.
+* Created by Luca Baumann, 2020.
+*/
+
 #include "tinystepperdriver.hh"
 
-tinystepperdriver::tinystepperdriver(int pin_step, int pin_dir)
+TinyStepperDriver::TinyStepperDriver(int pin_step, int pin_dir)
 {
-    tinystepperdriver::pin_step = pin_step;
-    tinystepperdriver::pin_dir = pin_dir;
+    TinyStepperDriver::pin_step = pin_step;
+    TinyStepperDriver::pin_dir = pin_dir;
 }
 
-tinystepperdriver::~tinystepperdriver()
+TinyStepperDriver::~TinyStepperDriver()
 {
 }
 
-bool tinystepperdriver::move(int steps, int direction, int duration)
+bool TinyStepperDriver::move(int steps, int direction, int duration)
 {
     if (active)
     {
+        // If there is a move currently running, refuse new configuration since no pipeline is defined
         return false;
     }
     else
@@ -22,27 +28,30 @@ bool tinystepperdriver::move(int steps, int direction, int duration)
         if (steps > duration) return false; // Assuming maximum speed of 1 step/millisecond
 
         // Initialize executing move
-        tinystepperdriver::active = true;
-        tinystepperdriver::steps = steps * 2; // steps is used to enable and disable step output pin
-        tinystepperdriver::direction = direction;
-        tinystepperdriver::duration = duration;
-        tinystepperdriver::last_step_time = millis();
-        tinystepperdriver::last_step_pin = false;
+        TinyStepperDriver::active = true;
+        TinyStepperDriver::steps = steps * 2; // steps is used to enable and disable step output pin, so double count is needed
+        TinyStepperDriver::direction = direction;
+        TinyStepperDriver::duration = duration * 1000; // convert to microseconds
+        TinyStepperDriver::last_step_time = micros();
+        TinyStepperDriver::last_step_pin = false;
 
-        // write direction
+        // Set direction
         digitalWrite(pin_dir, direction);
-
-        return true;
     }
     
+#ifdef TINYSTEPPERDRIVER_PYTHON_BIND
+    general_log("move," + std::to_string(TinyStepperDriver::steps) + "," + std::to_string(TinyStepperDriver::direction) + "," + std::to_string(TinyStepperDriver::duration) + ",,");
+#endif
+    
+    return true;
 }
 
-bool tinystepperdriver::iterate()
+bool TinyStepperDriver::iterate()
 {
     if (active)
     {
         // test for end condition
-        if (cancel_current || (duration <= 0 && steps <= 0))
+        if (cancel_current || (steps <= 0 && duration <= 0))
         {
             cancel_current = false;
 
@@ -52,13 +61,21 @@ bool tinystepperdriver::iterate()
 
             // disable execution
             active = false;
+            return true;
         }
 
         // check if the timing condition for a step is met
-        unsigned long time_since_step = millis() - last_step_time;
-
-        if (time_since_step > (unsigned long)duration / steps)
+        unsigned long time_since_step = micros() - last_step_time;
+        unsigned long wanted_duration = duration;
+        if (steps > 0)
         {
+            wanted_duration = duration / (unsigned long)steps;
+        }
+        
+
+        if (time_since_step >= wanted_duration)
+        {
+
             // do step if steps to do
             if (steps > 0)
             {
@@ -68,19 +85,24 @@ bool tinystepperdriver::iterate()
 
             // update variables
             steps--;
-            duration -= time_since_step;
-            last_step_time = millis();
+            last_step_time = micros();
+            if (duration < time_since_step) duration = 0;
+            else duration -= time_since_step;
+
+#ifdef TINYSTEPPERDRIVER_PYTHON_BIND
+            general_log("iterate," + std::to_string(TinyStepperDriver::steps) + "," + std::to_string(TinyStepperDriver::duration) + "," + std::to_string(TinyStepperDriver::last_step_time) + "," + std::to_string(time_since_step) + "," + std::to_string(wanted_duration));
+#endif
         }
         return true;
     }
     else return false;
 }
 
-void tinystepperdriver::cancel()
+void TinyStepperDriver::cancel()
 {
-    if (tinystepperdriver::active)
+    if (TinyStepperDriver::active)
     {
-        tinystepperdriver::cancel_current = true;
+        TinyStepperDriver::cancel_current = true;
     }
 }
 
--- tinystepperdriver.hh
+++ tinystepperdriver.hh
@@ -1,41 +1,60 @@
 /*
-* tinystepperdriver.h - Lightweight library for stepper motor controll on arduino
-* Created by Luca Baumann.
+* tinystepperdriver.hh - Lightweight library for stepper motor controll on arduino.
+* Created by Luca Baumann, 2020.
 */
 
 #ifndef tinystepperdriver_h
 #define tinystepperdriver_h
 
-#include "Arduino.h"
+//#include "Arduino.h"
+#include <Arduino.h>
 
-class tinystepperdriver
+class TinyStepperDriver
 {
 private:
+    // Store pins for step and direction outputs
     int pin_step, pin_dir = 0;
-    int steps, direction, duration = 0;
+    int steps, direction = 0;
     bool active, cancel_current, last_step_pin = false;
-    unsigned long last_step_time = 0;
+    unsigned long last_step_time, duration = 0;
+
 public:
-    tinystepperdriver(int, int);
-    ~tinystepperdriver();
+    /*
+    * Constructor for TinyStepperDriver
+    * pin_step: Pin number top write step operations
+    * pin_dir: Direction pin
+    */
+    TinyStepperDriver(int pin_step, int pin_dir);
+    ~TinyStepperDriver();
 
     /*
     * Initiates a move operation. 
+    * steps: Number of steps to be performed by setting the correlating pin to high and low
+    * direction: Translates directly to writing to the direction pin
+    * duration: Planned duration of the move in milliseconds
     */
-    bool move(int, int, int);
+    bool move(int steps, int direction, int duration);
+
+    /*
+    * Performs the move configured by move(). 
+    */
     bool iterate();
+    
+    /*
+    * Stops the current move on the next call of iterate(). 
+    */
     void cancel();
 };
 
-#ifdef C_LIB
+#ifdef TINYSTEPPERDRIVER_PYTHON_BIND
 /*
 * Bindings to C for python compatibility
 */
 extern "C" {
-    tinystepperdriver* tinystepperdriver_new(int pin_step, int pin_dir){ return new tinystepperdriver(pin_step, pin_dir); }
-    bool tinystepperdriver_move(tinystepperdriver* drv, int steps, int direction, int duration){ return drv->move(steps, direction, duration); }
-    bool tinystepperdriver_iterate(tinystepperdriver* drv){ return drv->iterate(); }
-    void tinystepperdriver_cancel(tinystepperdriver* drv){ return drv->cancel(); }
+    TinyStepperDriver* tinystepperdriver_new(int pin_step, int pin_dir){ return new TinyStepperDriver(pin_step, pin_dir); }
+    bool tinystepperdriver_move(TinyStepperDriver* drv, int steps, int direction, int duration){ return drv->move(steps, direction, duration); }
+    bool tinystepperdriver_iterate(TinyStepperDriver* drv){ return drv->iterate(); }
+    void tinystepperdriver_cancel(TinyStepperDriver* drv){ return drv->cancel(); }
 }
 
 #endif
